7638131b8e50d9231f1fac49321682fe52b787a6
Yining Wang <wyn.wang@mail.utoronto.ca>
2022-02-07 13:56:41 -0500

    Update test

--- test.py
+++ test.py
@@ -17,19 +17,19 @@ class TestDumpConvert(unittest.TestCase):
     def test_convert_Albedo(self):
         with open("test/Albedo.txt", "r") as t:
             with open("test/Albedo.html", "r") as h:
-                result_Albedo = convert(t.read().replace("\n", ""), self.tex_path)
+                result_Albedo = convert(t.read().replace("\n", ""), self.tex_path)[0]
                 self.assertEqual(result_Albedo, h.read().replace("\n", ""))
 
     def test_convert_Ampere(self):
         with open("test/Ampere.txt", "r") as t:
             with open("test/Ampere.html", "r") as h:
-                result_Ampere = convert(t.read().replace("\n", ""), self.tex_path)
+                result_Ampere = convert(t.read().replace("\n", ""), self.tex_path)[0]
                 self.assertEqual(result_Ampere, h.read().replace("\n", ""))
 
     def test_convert_Arithmetic_mean(self):
         with open("test/Arithmetic_mean.txt", "r") as t:
             with open("test/Arithmetic_mean.html", "r") as h:
-                result_Arithmetic_mean = convert(t.read().replace("\n", ""), self.tex_path)
+                result_Arithmetic_mean = convert(t.read().replace("\n", ""), self.tex_path)[0]
                 self.assertEqual(result_Arithmetic_mean, h.read().replace("\n", ""))
 
     def tearDown(self) -> None:
