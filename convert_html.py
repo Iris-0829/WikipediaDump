7849793940b79b8842e751cfac93574df47149e4
Yining Wang <wyn.wang@mail.utoronto.ca>
2022-02-08 16:04:09 -0500

    Add parsoid function

--- convert_html.py
+++ convert_html.py
@@ -1,8 +1,9 @@
-import os
+import subprocess
 
 def convert_to_html(file_name:str):
-    with open(file_name, 'w') as f:
-        content = f.read().replace("\n", "")
-        page_title = file_name[:file_name.find('.')] + "_html"
-        os.system(
-            'var="' + content + '" && printf \'%s\n\' \"$var\" | node node_modules/parsoid/bin/parse.js > result/' + page_title + '.html')
+    ps = subprocess.run(['cat', file_name], check=True, capture_output=True)
+    processNames = subprocess.run(['node', 'node_modules/parsoid/bin/parse.js'], input=ps.stdout, capture_output=True)
+    return processNames.stdout.decode('utf-8')
+
+# print(convert_to_html("test/Albedo.html"))
+
