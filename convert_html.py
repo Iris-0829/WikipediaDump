73a827c4f2a1eaeeb9c6e324134ed73e73ac6d09
Yining Wang <wyn.wang@mail.utoronto.ca>
2022-02-14 16:46:35 -0500

    Add command line

--- convert_html.py
+++ convert_html.py
@@ -1,9 +1,12 @@
 import subprocess
+import sys
 
-def convert_to_html(file_name:str):
+
+def convert_to_html(file_name: str):
     ps = subprocess.run(['cat', file_name], check=True, capture_output=True)
     processNames = subprocess.run(['node', 'node_modules/parsoid/bin/parse.js'], input=ps.stdout, capture_output=True)
     return processNames.stdout.decode('utf-8')
 
-# print(convert_to_html("test/Albedo.html"))
 
+if __name__ == "__main__":
+    print(convert_to_html(sys.argv[1]))
