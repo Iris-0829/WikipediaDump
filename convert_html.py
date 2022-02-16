a9f11682f1a3785e483a1bb923356f9a7185dcd2
Yining Wang <wyn.wang@mail.utoronto.ca>
2022-02-16 14:20:30 -0500

    Update parsoid function

--- convert_html.py
+++ convert_html.py
@@ -5,7 +5,7 @@ import sys
 def convert_to_html(file_name: str):
     ps = subprocess.run(['cat', file_name], check=True, capture_output=True)
     processNames = subprocess.run(['node', 'node_modules/parsoid/bin/parse.js'], input=ps.stdout, capture_output=True)
-    return processNames.stdout.decode('utf-8')
+    return processNames.stdout.decode('utf-8').replace('&lt;', '<')
 
 
 if __name__ == "__main__":
